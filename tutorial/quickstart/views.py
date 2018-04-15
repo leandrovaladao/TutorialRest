f0cdb9df8c9e45864479b7bcffc479e8489c62f4
Leandro Valadao <costavaladaol@gmail.com>
2018-04-15 18:57:22 -0300

    Add starting project files

---
+++ tutorial/quickstart/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from tutorial.quickstart.serializers import UserSerializer, groupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+	"""
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('date_joined')
